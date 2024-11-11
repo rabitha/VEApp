9023bcaa7d6b8f155a00a24809238025d8634c3c
parna_deltacube-at-584800525462 <rabitha_n@mpp.co.in>
2024-11-11 10:21:52 +0530

    initial setup in ios

---
+++ VEApp/VEApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VEApp
+//
+//  Created by Rabitha N. on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
